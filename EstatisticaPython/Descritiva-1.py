f8a80b7b0a46c5e83c4785f5c981da057a4351b0
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2025-05-20 23:19:44 -0300

    Create Descritiva-1.py

---
+++ EstatisticaPython/Descritiva-1.py
@@ -0,0 +1,5 @@
+# Estatística descritiva I: conceitos de probabilidade
+# https://caixa.ada.tech/cursos/1ee9f40f-0769-6290-888f-6d652778c0bc/modulo/1ee9f40f-0769-6291-fafa-7c653a74ab77/topico/1ee9f40f-0fd9-6ce0-ca65-45ca5cfafe68?backTitle=o+Curso&backUrl=%2Fcursos%2F1ee9f40f-0769-6290-888f-6d652778c0bc%3F
+
+# https://drive.google.com/drive/folders/1W2aDovLcooxKsQlMdFQ18hhiHoo3YkZI
+
